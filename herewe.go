0e6acaa98743573dbfcd2048c945a51b3de1929e
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-08-19 20:21:45 +0200

    zwischenstand

--- herewe.go
+++ herewe.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"net/http"
 )
 
 var Figures = map[string]figure{
@@ -28,32 +29,74 @@ type player struct {
 }
 
 func main() {
+	fmt.Println("Choose your playmode (single, server, client):")
+	
+	playmode := parseConsoleInput()
 
+	switch playmode {
+	case "server":
+		startServer()
+	case "client":
+		startClient()
+	case "single":
+		startSinglePlayer()
+	}
+}
+
+func startServer() {
+	// todo
+	fmt.Println("You started a server")
+	fmt.Println("One player can now join your server, so you can play against each other")
+
+	openWebserver()
+}
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+    
+
+    fmt.Println("Hey, someone registered, now you have to play!")
+
+    owner := getPlayerFromConsole()
+    url := string(request.URL.Path[1:])
+    opponentName := strings.Split(url, "/")[0]
+    opponentFigure := strings.Split(url, "/")[1]
+    opponent := player{opponentName, Figures[opponentFigure]}
+    winner := determineWinner(&owner, &opponent)
+
+	fmt.Println(owner.name, "picked", owner.figure.name)
+	fmt.Println(opponent.name, "picked", opponent.figure.name)
+	printWinnerText(winner)
+
+	fmt.Fprintf(writer, "Winner is", winner.name)
+}
+
+func openWebserver() {
+	http.HandleFunc("/", handler)
+    http.ListenAndServe(":5000", nil)
+}
+
+func startClient() {
+	// todo
+}
+
+func startSinglePlayer() {
 	computer := getComputerPlayer()
 	human := getPlayerFromConsole()
 
 	fmt.Println(human.name, "picked", human.figure.name)
 	fmt.Println(computer.name, "picked", computer.figure.name)
 
-	var winner = determineWinner(&human, &computer)
-	if winner != nil {
-		fmt.Println("Winner is", winner.name, winner.figure.ascii)
-	} else {
-		fmt.Println("It's a tie!")
-	}
+	winner := determineWinner(&human, &computer)
+	printWinnerText(winner)
 }
 
 func getFigureFromConsole() figure {
-	reader := bufio.NewReader(os.Stdin)
 
 	var userFigure figure
 	for userFigure == Figures["no"] {
 		fmt.Println("Please draw (rock, scissor, paper)")
-		userFigureName, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-		userFigureName = strings.TrimSpace(userFigureName)
+		userFigureName := parseConsoleInput()
+
 		userFigure = Figures[userFigureName]
 		if userFigure == Figures["no"] {
 			fmt.Println("Choose again! Your choice has not been recognized")
@@ -63,14 +106,9 @@ func getFigureFromConsole() figure {
 }
 
 func getPlayerFromConsole() player {
-	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println("Please choose a name")
-	userName, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	userName = strings.TrimSpace(userName)
+	userName := parseConsoleInput()
 	userFigure := getFigureFromConsole()
 
 	return player{userName, userFigure}
@@ -82,7 +120,6 @@ func getComputerPlayer() player {
 	for k := range Figures {
 		keys = append(keys, k)
 	}
-	fmt.Println(keys)
 	randomKey := keys[rand.Intn(len(Figures))]
 	return player{"Computer", Figures[randomKey]}
 }
@@ -95,5 +132,24 @@ func determineWinner(player1, player2 *player) *player {
 		return player2
 	} else {
 		return nil
+	}
+}
+
+
+func parseConsoleInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.TrimSpace(input)
+}
+
+func printWinnerText(winner *player) {
+	if winner != nil {
+		fmt.Println("Winner is", winner.name, winner.figure.ascii)
+	} else {
+		fmt.Println("It's a tie!")
 	}
 }
