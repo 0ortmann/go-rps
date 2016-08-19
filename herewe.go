ec48930ff29af1b78f6d49afeafc0adf0f5c7bc8
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-08-19 17:20:47 +0200

    work it

--- herewe.go
+++ herewe.go
@@ -8,22 +8,31 @@ import (
 )
 var Figures = [3]string { "rock", "paper", "scissor" }
 
+type player struct {
+	name string
+	figure string
+}
+
 func main() {
 
 	rand.Seed(time.Now().UTC().UnixNano())
-	playerFigure := os.Args[1] // bam if not provided
+	human := player{"You", os.Args[1]} // bam if not provided
 
-	randFigure := Figures[rand.Intn(len(Figures))]
-
-
-	fmt.Println("You (player 1)", playerFigure)
-	fmt.Println("Computer (player 2)", randFigure)
+	computer := player{"Computer", Figures[rand.Intn(len(Figures))]}
 
 
-	fmt.Println("winner is player", determineWinner(playerFigure, randFigure))
+	fmt.Println(human.name, "picked", human.figure)
+	fmt.Println(computer.name, "picked", computer.figure)
+
+	var winner = determineWinner(&human, &computer)
+	if winner != nil {
+		fmt.Println("Winner is", winner.name)
+	} else {
+		fmt.Println("It's a tie!")
+	}
 }
 
-func determineWinner(figures ...string) string {
+func determineWinner(player1, player2 *player) *player {
 
 	successors := map[string]string {
 		"paper": "scissor",
@@ -31,11 +40,11 @@ func determineWinner(figures ...string) string {
 		"rock": "paper",
 	}
 
-	if successors[figures[0]] == figures[1] {
-		return "2"
-	} else if successors[figures[1]] == figures[0] {
-		return "1"
+	if successors[player1.figure] == player2.figure {
+		return player2
+	} else if successors[player2.figure] == player1.figure {
+		return player1
 	} else {
-		return "draw"
+		return nil
 	}
 }
