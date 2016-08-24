f47cbfa554ed1e2faf72c58991f37f86493474c7
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-08-24 02:45:31 +0200

    iterative bot

--- bot.go
+++ bot.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"net/http"
-  "math/rand"
-  "io/ioutil"
+	"strconv"
+	"sync"
+	"time"
 )
 
 type Bot struct {
@@ -18,51 +23,131 @@ type GameStore struct {
 	mu    sync.RWMutex
 }
 
-gs := NewGameStore()
-playURL := "localhost:5000/play"
+func NewBot(name string) *Bot {
+	return &Bot{
+		Name: name,
+	}
+}
+
+func NewGameStore() *GameStore {
+	return &GameStore{}
+}
+
+var gs = NewGameStore()
+
+const playURL = "http://localhost:5000/play"
+const createURL = "http://localhost:5000/create"
+const evalURL = "http://localhost:5000/eval"
 
 func main() {
+	initGameStore()
 
+	for i := 0; i < len(gs.games); i++ {
+		initBots()
+	}
+	time.Sleep(time.Second)
 }
 
-func NewGameStore()  *GameStore{
-  return &GameStore{}
+// Creates a random number of bots between 1 and 10 and lets them play a game, then close that game
+func initBots() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	game := gs.Get()
+	sendCreate(game)
+	for i := 0; i < rand.Intn(10); i++ {
+		b := NewBot("bot-" + strconv.Itoa(i))
+		b.Play()
+	}
+	time.Sleep(time.Second)
+	sendEval(game)
+	gs.CloseGame()
 }
 
-
-func init()  {
-
+func initGameStore() {
+	for i := 0; i < 5; i++ {
+		name := "game-" + strconv.Itoa(i+1)
+		fmt.Println("Created game: ", name)
+		gs.Set(name)
+	}
 }
 
-func (gs *GameStore) CloseGame(g string) {
+func (gs *GameStore) CloseGame() {
 	gs.mu.Lock()
-  defer gs.mu.Unlock()
-  delete(gs.games[g])
+	defer gs.mu.Unlock()
+	gs.games[0] = gs.games[len(gs.games)-1]
+	gs.games = gs.games[:len(gs.games)-1]
 }
 
-func (gs *GameStore) Get()  {
-  gs.mu.RLock()
-  defer gs.mu.RUnlock()
-  return gs.games[0]
+func (gs *GameStore) Get() string {
+	gs.mu.RLock()
+	defer gs.mu.RUnlock()
+	return gs.games[0]
+}
+
+func (gs *GameStore) Set(game string) {
+	gs.mu.Lock()
+	defer gs.mu.Unlock()
+	gs.games = append(gs.games, game)
 }
 
 func (b *Bot) Play() {
-  g := gs.Get()
-  json := []byte(`{"game" : g, "player": b.Name, "action" : b.ChooseAction()}`)
-  resp, err := http.Post(playURL, "application/json", bytes.NewBuffer(json))
-    if err != nil {
-    fmt.Println("I faild to play :( )")
-    return
-  }
-  defer resp.Body.Close()
+	type Payload struct {
+		Game   string
+		Player string
+		Action string
+	}
+	payload := &Payload{gs.Get(), b.Name, b.ChooseAction()}
+	jsonStr, _ := json.Marshal(payload)
 
-  body,_ := ioutil.ReadAll(resp.Body)
-  fmt.Println(body)
+	fmt.Println(string(jsonStr))
 
+	resp, err := http.Post(playURL, "application/json", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		fmt.Println("I faild to play :( )", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
 
 func (b *Bot) ChooseAction() string {
-  rand.Seed(time.Now().UTC().UnixNano())
-  a := [3]string{"rock","paper", "scissor"}
-  return a[rand.Intn(3)]
+	rand.Seed(time.Now().UTC().UnixNano())
+	a := [3]string{"rock", "paper", "scissor"}
+	return a[rand.Intn(3)]
+}
+
+func sendEval(game string) {
+	type Payload struct {
+		Game string
+	}
+	payload := &Payload{game}
+	jsonStr, _ := json.Marshal(payload)
+	resp, err := http.Post(evalURL, "application/jsonStr", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		fmt.Println("I faild to eval for game", game)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
+
+func sendCreate(game string) {
+	fmt.Println("Shall send create for ", game)
+	type Payload struct {
+		Name string
+	}
+	payload := &Payload{game}
+	jsonStr, _ := json.Marshal(payload)
+	resp, err := http.Post(createURL, "application/jsonStr", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		fmt.Println("I faild to create game", game)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
